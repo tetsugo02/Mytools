b019d31cdd0d4527efdfde8b64095cd44c14cc97
Linux-tetsugo <tetsugou2002@gmail.com>
2024-01-10 02:33:38 +0900

    changed the data type of Subject::grade

--- c++/GPA/Subjects.h
+++ c++/GPA/Subjects.h
@@ -1,7 +1,6 @@
 
 #ifndef SUBJECT_H
 #define SUBJECT_H
-#include <string>
 #include <iostream>
 using namespace std;
 
@@ -9,27 +8,28 @@ using namespace std;
 class Subject{
     public:
         Subject();
-        Subject(string name,string teacher ,int grade);
+        Subject(string name,string teacher ,string grade);
         void setName(string name_set);
         void setTeacher(string teacher_set);
-        void setGrade(int grade_set);
+        void setGrade(string grade_set);
+        void showGrade();
     protected:
         string name;
         string teacher;
-        int grade;
+        string grade;
 };
 
 Subject::Subject()
 {
     cout<<"Subject created"<<endl;
 }
-Subject::Subject(string name,string teacher ,int grade)
+Subject::Subject(string name,string teacher ,string grade)
 {
     this->name=name;
     this->teacher=teacher;
     this->grade=grade;
 }
-//授業の名前、先生、成績を設定する
+//授業の名前、先生、成績、成績を設定する
 void Subject::setName(string name_set)
 {
     name=name_set;
@@ -38,10 +38,15 @@ void Subject::setTeacher(string teacher_set)
 {
     teacher=teacher_set;
 }
-void Subject::setGrade(int grade_set)
+void Subject::setGrade(string grade_set)
 {
     grade=grade_set;
 }
-
+void Subject::showGrade()
+{
+    cout<<"Subject name: "<<name<<endl;
+    cout<<"Subject teacher: "<<teacher<<endl;
+    cout<<"Subject grade: "<<grade<<endl;
+}
 
 #endif // SUBJECT_H
--- c++/GPA/main.cpp
+++ c++/GPA/main.cpp
@@ -2,5 +2,15 @@
 #include "Subjects.h"
 using namespace std;
 
+int main()
+{
+    Subject subject1;
+    subject1.setName("Math");
+    subject1.setTeacher("Mr. Smith");
+    subject1.setGrade("A");
+    subject1.showGrade();
+    return 0;
+}
+
 
 
